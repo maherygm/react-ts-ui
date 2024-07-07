9a84d399527e1f21fd9ec48d3604e244fedaa1ca
mahery <maheryrak1234@gmail.com>
2024-07-07 14:02:20 +0300

    'shad-cn-ui'

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,27 @@
-import clsx from "clsx";
-import React, { ComponentPropsWithoutRef } from "react";
-
-export type ButtonPros = ComponentPropsWithoutRef<"button"> & {
-  variant?: "primary" | "secondary";
-};
-
-const Button = ({ variant = "primary", className, ...props }: ButtonPros) => {
-  return (
-    <button
-      className={clsx(
-        {
-          "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded":
-            variant === "primary",
-          "bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded":
-            variant === "secondary",
-        },
-        className
-      )}
-      {...props}
-    >
-      {props.children}
-    </button>
-  );
-};
-
+import { Button } from "@/@components/ui/button";
+import { Calendar } from "@/@components/ui/calendar";
+import React from "react";
 export default function App(): React.ReactElement {
   const [counter, setCounter] = React.useState(0);
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
     <>
-      <div className="flex flex gap-4 items-center justify-center h-screen">
-        <Button onClick={() => setCounter(counter + 1)}>+1</Button>
+      <div className="flex  flex-col items-center justify-center h-screen gap">
+        <div className="flex flex gap-4 items-center justify-center ">
+          <Button onClick={() => setCounter(counter + 1)}>+1</Button>
 
-        <p>{counter}</p>
-        <Button variant="secondary" onClick={() => setCounter(counter - 1)}>
-          -1
-        </Button>
+          <p>{counter}</p>
+          <Button variant="secondary" onClick={() => setCounter(counter - 1)}>
+            -1
+          </Button>
+        </div>
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border h-fit"
+        />
       </div>
     </>
   );
